26911f415a556b3bf491e8d2f28c9fae39b2fda1
Marissa Schmidt <marissaschmidt@boisestate.edu>
2018-02-22 18:47:28 -0700

    Adding starter files for Activity 6

---
+++ activities/a6/Orbit.java
@@ -0,0 +1,106 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Animates and object orbiting around the Earth.
+ *
+ * @author CS121 Instructors
+ * @author <you>
+ */
+@SuppressWarnings("serial")
+public class Orbit extends JPanel
+{
+	private final int INIT_WIDTH = 600;
+	private final int INIT_HEIGHT = 600;
+	private final int DELAY = 100; //milliseconds
+	
+	private final ImageIcon EARTH_ICON = new ImageIcon("earth.png");
+	
+	private int orbitTheta;
+	private int orbitTDelta;
+	
+	/**
+	 * Constructor. Sets the initial dimensions and starts the animation.
+	 */
+	public Orbit()
+	{
+		setBackground(Color.white);
+		setPreferredSize(new Dimension(INIT_WIDTH, INIT_HEIGHT));
+		
+		startAnimation();
+		
+		orbitTheta = 0;
+		
+		// TODO: Generate random theta delta value from -10 to 20.
+		orbitTDelta = 10;
+	}
+
+	/**
+	 * Display the square at a new location.
+	 * @param g Graphics context
+	 */
+	public void paintComponent(Graphics g)
+	{
+		int width = getWidth();
+		int height = getHeight();
+		
+		// Define earth radius and position.
+		int earthRadius = Math.min(width/5, height/5);
+		int earthX = width / 2;
+		int earthY = height / 2;
+		
+		// Draw earth
+		g.drawImage(EARTH_ICON.getImage(), earthX-earthRadius, earthY-earthRadius, earthRadius*2, earthRadius*2, null);
+		
+		// TODO: Define orbit radius
+		
+		// TODO: Draw orbit path
+		
+		// TODO: Define the radius of your object
+		
+		// TODO: Calculate x and y using Math.sin and Math.cos.
+		
+		// TODO: Draw your object as an oval with a random color.
+		
+		// Add to theta.
+		orbitTheta = orbitTheta + orbitTDelta;
+		
+		// Make the animation smoother
+		Toolkit.getDefaultToolkit().sync();
+	}
+
+	/**
+	 * sets up a JFrame and the RandomMovement panel
+	 * @param args unused
+	 */
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("Geocentric Orbit");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new Orbit());
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Create an animation thread that runs periodically
+	 */
+	private void startAnimation()
+	{
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				repaint();
+			}
+		};
+		new Timer(DELAY, taskPerformer).start();
+	}
+}

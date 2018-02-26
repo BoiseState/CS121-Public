ad1a9cb336619caf7b2480167c6d10f9cd46dcd2
Marissa Schmidt <marissaschmidt@boisestate.edu>
2018-02-26 10:15:07 -0700

    Activity 11: Importing starter file.

---
+++ activities/a11/CheckerBoard.java
@@ -0,0 +1,56 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ * Lesson 11: Activity - for Loops
+ * 
+ * @author CS121 Instructors
+ * @version Fall 2018
+ */
+@SuppressWarnings("serial")
+public class CheckerBoard extends JPanel
+{
+	private static final int NUM_ROWS = 8;
+	private static final int NUM_COLS = 8;
+
+	/**
+	 * Sets the initial dimensions of the panel. 
+	 */
+	public CheckerBoard()
+	{
+		setPreferredSize(new Dimension(500, 500));
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
+	public void paintComponent(Graphics page)
+	{
+		int x = 0, y = 0;
+
+		int width = getWidth();
+		int height = getHeight();
+
+		int boxWidth = width/NUM_ROWS;
+		int boxHeight = height/NUM_COLS;
+
+		// TODO: Your code here.	
+	}
+
+	/**
+	 * Creates the main frame of the program.
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("Checker Board");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		CheckerBoard panel = new CheckerBoard();
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}

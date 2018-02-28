3c786eb0fc621e11a781885b3b6ed95c6ebbc2ed
Marissa Schmidt <marissaschmidt@boisestate.edu>
2018-02-28 11:30:46 -0700

    Activity 04: Initial import

---
+++ activities/a04/MyAvatar.java
@@ -0,0 +1,93 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Color;
+
+/**
+ * Lesson 4: Activity - Using Classes and Objects
+ * 
+ * Uses the MiniFig class to draw a custom avatar.
+ * 
+ * @author CS121 instructors
+ * @author <you>
+ */
+@SuppressWarnings("serial")
+public class MyAvatar extends JPanel
+{
+	public final int INITIAL_WIDTH = 800;
+	public final int INITIAL_HEIGHT = 600;
+	
+	/**
+	 * Draws the picture in the panel. This is where all of your
+	 * code will go.
+	 * @param g Our graphics canvas.
+	 */
+	public void paintComponent (Graphics g)
+	{
+		/* Store the height and width of the panel at the time
+		 * the paintComponent() method is called.
+		 */
+		int currentHeight = getHeight();
+		int currentWidth = getWidth();
+		
+		/* This is the anchor point for the MiniFig (x,y) -> (mid,top) */
+		int mid = currentWidth / 2;
+		int top = 50;
+		
+		/* This is the scaler that is used to calculate the dimensions (height / width) 
+		 * of each of the MiniFig components. It uses the Math.min() function to select
+		 * the smaller of currentWidth/INITIAL_WIDTH and currentHeight/INITIAL_HEIGHT.
+		 * This way all the components are scaled to fit within the smaller of the two 
+		 * panel dimensions.
+		 */
+		double scaleFactor = Math.min(currentWidth/(double)INITIAL_WIDTH,currentHeight/(double)INITIAL_HEIGHT );
+
+		// TODO: 1. Instantiate a new Point object called "anchor". Use "mid" as your x value and
+		//       "top" as your y value.
+		
+		// TODO: 2. Instantiate a new MiniFig object. Use the MiniFig constructor with the following
+		//       parameters: MiniFig(Graphics canvas, double scaleFactor, Point anchor)
+		
+		// TODO: 3. Create a new Color object.
+		
+		// TODO: 4. Invoke the setTorsoColor(Color color) method on your MiniFig instance.
+		//          Use your color object as a parameter to change the shirt color.
+		
+		// TODO: 5. Invoke the draw() method on your MiniFig instance.
+		
+		// TODO: 6: Use the getBaseMidPoint() method to find the the base mid point of your
+		//       avatar. Replace the hard-coded value of grassYOffset with the y value 
+		//       of the returned point.
+		int grassYOffset = 400;
+		
+		Color grassGreen = new Color (60,80,38);
+		g.setColor(grassGreen);
+		g.fillRect(0, grassYOffset, getWidth(), getHeight() - grassYOffset);
+	}
+
+
+	/**
+	 * Constructor (panel initialization)
+	 */
+	public MyAvatar()
+	{
+		this.setBackground(Color.white);
+		this.setPreferredSize(new Dimension(INITIAL_WIDTH, INITIAL_HEIGHT));
+	}
+
+	/**
+	 * Sets up a JFrame and the MiniFigDriver panel.
+	 * @param args unused
+	 */
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("MyAvatar");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new MyAvatar());
+		frame.pack();
+		frame.setVisible(true);
+	}
+}

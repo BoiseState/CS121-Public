e6a46fb8c6c26d0abd2e51623db32b21425929e1
Luke Hindman <lukehindman@boisestate.edu>
2018-03-26 16:20:29 -0600

    Added TODO numbers

--- activities/a13/GradeBook.java
+++ activities/a13/GradeBook.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
- * Lesson 13: Activity - Gradebook
+ * Lesson 13: Activity - GradeBook
  *
  * @author CS121 Instructors
  * @version [semester]
@@ -13,35 +13,35 @@ public class GradeBook {
 
 	public static void main(String[] args) {
 		
-		/* TODO: Create a new ArrayList of Student objects called gradebook. */
+		/* TODO: 1. Create a new ArrayList of Student objects called gradebook. */
 		
 		
-		/* TODO: Create a new File object for gradebook.csv and 
+		/* TODO: 2. Create a new File object for gradebook.csv and 
 		 * a new Scanner object to parse it. Catch any required 
 		 * exceptions and display a useful message to the user.
 		 */
 
 				
-			/* TODO: Use a while loop to iterate through the lines in
-			 * the gradebook.csv file.
+			/* TODO: 3. Use a while loop and the Scanner created above to iterate 
+			 * through the lines in the gradebook.csv file.
 			 */
 
 
-				/* TODO:  For each line (student record), use a second 
-				 * scanner object to tokenize the line using a ',' as the
-				 * delimiter, extract values for lastName, firstName, id 
-				 * and grade and store them to local variables.
+				/* TODO: 4. For each line (student record), use a second 
+				 * Scanner object to tokenize the line using a comma (',')
+				 * as the delimiter, extract values for lastName, firstName,
+				 * id and grade and store them to local variables.
 			 	 */
 
 
-			 	/* TODO: Create a new Student object using the local variables
+			 	/* TODO: 5. Create a new Student object using the local variables
 			 	 * create above, set the student's grade, and finally add the 
 			 	 * new Student object to the gradebook ArrayList. 
 				 */
 
 		
 		
-		/* TODO: Use a foreach loop to print out contents of the gradebook */
+		/* TODO: 6. Use a foreach loop to print out contents of the gradebook */
 
 		
 

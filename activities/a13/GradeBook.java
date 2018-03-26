5a30519fa9e19dfaf68cd4770b2610fe88eefe86
Luke Hindman <lukehindman@boisestate.edu>
2018-03-26 14:35:44 -0600

    Added Activity 13

---
+++ activities/a13/GradeBook.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ * Lesson 13: Activity - Gradebook
+ *
+ * @author CS121 Instructors
+ * @version [semester]
+ * @author [your name]
+ */
+public class GradeBook {
+
+	public static void main(String[] args) {
+		
+		/* TODO: Create a new ArrayList of Student objects called gradebook. */
+		
+		
+		/* TODO: Create a new File object for gradebook.csv and 
+		 * a new Scanner object to parse it. Catch any required 
+		 * exceptions and display a useful message to the user.
+		 */
+
+				
+			/* TODO: Use a while loop to iterate through the lines in
+			 * the gradebook.csv file.
+			 */
+
+
+				/* TODO:  For each line (student record), use a second 
+				 * scanner object to tokenize the line using a ',' as the
+				 * delimiter, extract values for lastName, firstName, id 
+				 * and grade and store them to local variables.
+			 	 */
+
+
+			 	/* TODO: Create a new Student object using the local variables
+			 	 * create above, set the student's grade, and finally add the 
+			 	 * new Student object to the gradebook ArrayList. 
+				 */
+
+		
+		
+		/* TODO: Use a foreach loop to print out contents of the gradebook */
+
+		
+
+	}
+
+}
---
+++ activities/a13/Student.java
@@ -0,0 +1,119 @@
+/**
+ * Lesson 13: Activity - Gradebook
+ *
+ * @author CS121 Instructors
+ * @version Fall 2018
+ */
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int id;
+	private int grade;
+	
+	/**
+	 * Create a new student object
+	 * @param firstName First name of student
+	 * @param lastName Last name of student
+	 * @param id Student's id number
+	 */
+	public Student(String firstName, String lastName, int id) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+		this.grade = -1;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the grade
+	 */
+	public int getGrade() {
+		return grade;
+	}
+
+	/**
+	 * @param grade the grade to set
+	 */
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	/**
+	 * Get a letter grade that represents the student's current score
+	 * @return String containing the letter grade representing the student's course grade
+	 */
+	public String getLetterGrade() {
+		
+		String letterGrade;
+		
+		if (this.grade >= 90) {
+			letterGrade = "A";
+		} else if (this.grade >= 80) {
+			letterGrade = "B";
+		} else if (this.grade >= 70) {
+			letterGrade = "C";
+		} else if (this.grade >= 60) {
+			letterGrade = "D";
+		} else if (this.grade >= 0) {
+			letterGrade = "F";
+		} else {
+			letterGrade = "No grades have been entered for student";
+		}
+		
+		return letterGrade;
+		
+	}
+	
+	@Override 
+	public String toString() {
+		String output = "";
+		
+		String letter = this.getLetterGrade();
+		output += this.firstName + " " + this.lastName + " has " + (letter.equals("A")?"an ":"a ") + letter + ".";
+		
+		return output;		
+	}
+	
+
+}

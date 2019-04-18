b335e7419abddb9bd4d3a8c3d8ce59ca02421c7a
Marissa Schmidt <marissaschmidt@boisestate.edu>
2019-04-18 08:15:18 -0600

    Modified GrowingArray example to read filename from command-line argument

--- examples/chap07/GrowingArray.java
+++ examples/chap07/GrowingArray.java
@@ -1,18 +1,21 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
  * Illustrates how to grow an array on the fly. Show the time taken when we grow
  * the array by 1 each time.
  *
- * time java GrowingArray < wordlist.txt 1
+ * time java GrowingArray wordlist.txt 1
  *
  * Then see the improvement if we double the capacity each time we grow the array. The
  * command line argument of 0 doubles the size each time.
  *
- * time java GrowingArray < wordlist.txt 0
+ * time java GrowingArray wordlist.txt 0
  * 
  * @author amit
+ * @author marissa (slight modifications)
  */
 
 public class GrowingArray
@@ -32,45 +35,63 @@ public class GrowingArray
 	 */
 	public GrowingArray(int initialCapacity, int increment)
 	{
-		words = new String[initialCapacity];
-		size = 0;
+		this.words = new String[initialCapacity];
+		this.size = 0;
 		this.increment = increment;
-	}
-
-	/**
-	 * Increase capacity of internal array.
-	 */
-	private void growArray()
-	{
-		int extra = increment; 
-		if (increment == 0) {
-			extra = words.length;
-		}
-		String[] temp = new String[words.length + extra];
-		for (int i = 0; i < words.length; i++) {
-			temp[i] = words[i];
-		}
-
-		words = temp;
-		System.out.println("Increased capacity to " + words.length);
 	}
 
 	/**
 	 * Read in words (one per line) and store in array words, growing it as needed.
 	 */
-	public void readData()
+	public void readData(File file)
 	{
-		Scanner input = new Scanner(System.in);
-
-		while (input.hasNext()) {
-			String line = input.nextLine();
-				if (size == words.length) {
+		try
+		{
+			Scanner input = new Scanner(file);
+			while(input.hasNextLine()) 
+			{
+				// read the next word (input is one word per line)
+				String word = input.nextLine();
+				
+				// if the array is full, increase capacity
+				if (size == words.length)
+				{
 					growArray();
 				}
-				words[size] = line;
+				
+				// store the word in the next available slot and increment size
+				words[size] = word;
 				size++;
+			}
+			input.close();
 		}
-		input.close();
+		catch (FileNotFoundException e)
+		{
+			System.err.println("File not found: " + file);
+		}
+	}
+	
+	/**
+	 * Increase capacity of internal words array.
+	 */
+	private void growArray()
+	{
+		int extra = increment;
+		if (increment == 0) {
+			extra = words.length; // double the size of the array
+		}
+		
+		// copy words into new temp array
+		String[] temp = new String[words.length + extra];
+		for (int i = 0; i < words.length; i++)
+		{
+			temp[i] = words[i];
+		}
+
+		// assign temp to words (aka words is now temp)
+		words = temp;
+		
+		System.out.println("Increased capacity to " + words.length);
 	}
 
 	/**
@@ -78,24 +99,32 @@ public class GrowingArray
 	 */
 	public void printData()
 	{
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++)
+		{
 			System.out.println(words[i]);
 		}
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-		if (args.length != 1) {
-			System.err.println("Usage: java GrowingArray <array increment>");
+		if (args.length != 2) {
+			System.err.println("Usage: java GrowingArray <filename> <array increment>");
 			System.exit(1);;
 		}
-		int increment = Integer.parseInt(args[0]);
+		
+		// Read filename from 1st command line argument
+		String filename = args[0];
+		File file = new File(filename);
+		if(!file.exists() || !file.canRead())
+		{
+			System.err.println("Cannot read file: " + file);
+			System.exit(1);;
+		}
+		
+		// Read increment from 2nd command line argument
+		int increment = Integer.parseInt(args[1]);
 		GrowingArray example = new GrowingArray(1, increment);
-		example.readData();
-		/* example.printData(); */
+		
+		example.readData(new File(filename));
 	}
-
 }

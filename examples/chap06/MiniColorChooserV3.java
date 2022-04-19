347bde5853fa1a10d2c65bc7755f795b56a7d95d
MasonVail <masonvail@onyx.boisestate.edu>
2022-04-19 07:52:22 -0600

    adding correct version of new MiniColorChooser

--- examples/chap06/MiniColorChooserV3.java
+++ examples/chap06/MiniColorChooserV3.java
@@ -14,12 +14,12 @@ import javax.swing.JPanel;
  * @author mvail
  */
 @SuppressWarnings("serial")
-public class MiniColorChooserV3 extends JPanel implements ActionListener {
+public class MiniColorChooserV3 extends JPanel {
 	private final int GRID_DIM = 3;
 	private JButton[][] colorButton;
 	private Color[][] color;
 	private JPanel displayPanel;
-	
+
 	/**
 	 * main panel constructor
 	 */
@@ -33,17 +33,18 @@ public class MiniColorChooserV3 extends JPanel implements ActionListener {
 		displayPanel.setPreferredSize(new Dimension(120,120));
 		//colors 
 		Color[][] c = {{Color.red, Color.green, Color.blue}, 
-				 {new Color(255, 255, 0), new Color(0, 255, 255), new Color(255, 0, 255)},
-				 {Color.white, Color.black, Color.gray}};
-		color = c; //necessary to use initializer list in constructor
-					//while also storing array as instance data
+				{new Color(255, 255, 0), new Color(0, 255, 255), new Color(255, 0, 255)},
+				{Color.white, Color.black, Color.gray}};
+		color = c;	//jumping through a hoop to use initializer list in
+					//constructor while also storing array as instance data
 		//buttons
 		colorButton = new JButton[GRID_DIM][GRID_DIM];
+		ButtonListener listener = new ButtonListener(); //color button listener
 		for (int row = 0; row < colorButton.length; row++) {
 			for (int col = 0; col < colorButton[row].length; col++) {
 				colorButton[row][col] = new JButton();
 				colorButton[row][col].setBackground(color[row][col]);
-				colorButton[row][col].addActionListener(this);
+				colorButton[row][col].addActionListener(listener); //assign listener to color buttons
 				gridPanel.add(colorButton[row][col]);
 			}
 		}
@@ -52,26 +53,29 @@ public class MiniColorChooserV3 extends JPanel implements ActionListener {
 		this.add(displayPanel);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		//find the row and col of the clicked button and set the
-		//display panel color to the corresponding color in the
-		//color array
-		for (int row = 0; row < colorButton.length; row++) {
-			for (int col = 0; col < colorButton[row].length; col++) {
-				if (e.getSource() == colorButton[row][col]) {
-					displayPanel.setBackground(color[row][col]);
+	/**
+	 * Responds to a color button click by finding the button's location in the
+	 * grid and updating the displayPanel to match that color.
+	 */
+	private class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			for (int row = 0; row < colorButton.length; row++) {
+				for (int col = 0; col < colorButton[row].length; col++) {
+					if (e.getSource() == colorButton[row][col]) {
+						displayPanel.setBackground(color[row][col]);
+					}
 				}
 			}
+			//NOTE: This is an unnecessarily inefficient way to find the
+			//button color. A loop like this only makes sense if you need
+			//to know the row and column of the button. In this case, we
+			//used the row and column values to index into the color array.
+			//The functionality of the loop above, however, can be 
+			//accomplished in two statements:
+			//JButton button = (JButton)(e.getSource());
+			//displayPanel.setBackground(button.getBackground());
 		}
-		//NOTE: This is an unnecessarily inefficient way to find the
-		//button color. A loop like this only makes sense if you need
-		//to know the row and column of the button. In this case, we
-		//used the row and column values to index into the color array.
-		//The functionality of the loop above, however, can be 
-		//accomplished in two statements:
-		//JButton button = (JButton)(e.getSource());
-		//displayPanel.setBackground(button.getBackground());
 	}
 
 	/**
@@ -81,9 +85,9 @@ public class MiniColorChooserV3 extends JPanel implements ActionListener {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Mini-ColorChooser");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		frame.getContentPane().add(new MiniColorChooserV3());
-		
+
 		frame.pack();
 		frame.setVisible(true);
 	}

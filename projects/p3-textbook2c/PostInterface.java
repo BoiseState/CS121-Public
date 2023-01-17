3c3d0d1209d0ab4a41416753437750d75a900a85
Mason Vail <mevail-BSU@users.noreply.github.com>
2023-01-17 14:36:35 -0700

    Initial files for textbook2c

---
+++ projects/p3-textbook2c/PostInterface.java
@@ -0,0 +1,104 @@
+import java.time.Instant;
+
+/**
+ * DO NOT MODIFY THIS INTERFACE - It is the responsibility of your Post class to
+ * provide methods that look and behave exactly as defined in this interface.
+ * 
+ * A Post represents a single text post in a TextBook social media site along with
+ * its associated comments. A Post must implement this interface as follows:
+ * 	 public class Post implements PostInterface
+ * 
+ * In addition to the methods listed for this interface, a Post is expected to
+ * have two constructors, one used to create a new Post and the other to recover 
+ * the post and any comments from an associated text file.
+ * The new Post constructor must have the following signature:
+ * 	 public Post(int id, String author, String text)
+ * The recovery constructor must have the following signature:
+ *   public Post(int id)
+ * 
+ * @author mvail, jerryfails
+ */
+public interface PostInterface {
+	
+	/**
+	 * Adds a comment with the given values and the current timestamp to this Post.
+	 * Appends comment timestamp, author, and text to the Post's associated file.
+	 * Example file contents with a post and two comments:
+	 *   00012 2010-08-30T13:38:23.085Z Sam Pondering the meaning of static.
+	 *   2010-08-30T15:21:43.055Z Pat Don’t get too existential.
+	 *   2010-08-31T09:15:12.118Z Jean Let me know if you get it.
+	 *
+	 * @param author one-word username of the author, e.g. "AdaLovelace"
+	 * @param text complete post text
+	 */
+	public void addComment(String author, String text);
+
+	/**
+	 * Returns a well-formatted String including the original post and all comments, 
+	 * each on their own lines. Intended for use when a specific post and all of its 
+	 * associated comments should be printed to console. Note that post ID should
+	 * be formatted to 5-digits as shown in the example. The timestamp conforms to
+	 * ISO-8601 standard as returned by an Instant's toString(). Note: "Comments:"
+	 * header should always be displayed, even if there are no comments on the post.
+	 * Example:
+	 * 	 Post:
+	 *   00010 2021-11-30T23:38:23.085Z Mason Did stuff.
+	 *   Comments:
+	 * 	 2021-12-01T08:18:21.055Z Luke It's late...
+	 * 	 2021-12-01T09:22:03.142Z Kathryn And it wasn't the right stuff. 
+	 * 
+	 * @return a well-formatted String including post and all comments 
+	 */
+	public String toString();
+	
+	/**
+	 * Returns a well-formatted String including post ID, date, author, and text of 
+	 * the original post, exactly as shown in the first line of toString() output.
+	 * Intended for use when a list of posts is printed to console and for writing
+	 * the first line to the first line of a new Post's associated file.
+	 * Example:
+	 * 	 00010 2021-11-30T23:38:23.085Z Mason Did stuff
+	 * 
+	 * @return well-formatted String including id, date, author, and post text only
+	 */
+	public String toStringPostOnly();
+	
+	/**
+	 * Return associated filename with format: Post-<postID>.txt
+	 * where postID is formatted to 5 digits as shown in the example.
+	 * Example:
+	 * 	Post-00010.txt
+	 * 
+	 * @return filename associated with post ID
+	 */
+	public String getFilename();
+	
+	/**
+	 * Return post ID.
+	 * 
+	 * @return post ID
+	 */
+	public int getPostID();
+	
+	/**
+	 * Return text of original post.
+	 * 
+	 * @return text of original post
+	 */
+	public String getText();
+	
+	/**
+	 * Return timestamp of original Post as Instant.
+	 * 
+	 * @return Instant of original post
+	 */
+	public Instant getTimestamp();
+	
+	/**
+	 * Return author of original Post.
+	 * 
+	 * @return author of original post
+	 */
+	public String getAuthor();
+
+}
---
+++ projects/p3-textbook2c/TextBookInterface.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+
+/**
+ * DO NOT MODIFY THIS INTERFACE - It is the responsibility of your TextBook class
+ * to provide methods that look and behave exactly as described in this interface.
+ * 
+ * A TextBook manages a collection of Posts for the TextBook social media site. 
+ * All Post IDs are recorded in a file with the name specified in constant
+ * POST_LIST_FILENAME. A TextBook must implement this interface as follows:
+ *   public class TextBook implements TextBookInterface
+ * 
+ * In addition to the methods listed below, a TextBook is expected to have a single
+ * constructor with no parameters. When instantiated, a TextBook attempts to restore 
+ * the previous session from files. If it is unable to do so, it initiates a new 
+ * empty list of Posts and sets the last post ID to zero. This constructor has
+ * signature:
+ *   public TextBook()
+ * 
+ * @author mvail, jerryfails
+ */
+public interface TextBookInterface {
+	/** 
+	 * This constant defines the expected name of the file that contains a
+	 * list of all Post IDs in the TextBook, one ID per line.
+	 */
+	public static final String POST_LIST_FILENAME = "posts.txt";
+	
+	/**
+	 * Return the last post ID assigned to a Post in TextBook. If TextBook has
+	 * just been loaded, this number corresponds to the last ID read from file 
+	 * POST_LIST_FILENAME or 0 if the file was not found.
+	 * 
+	 * @return current value of lastID
+	 */
+	public int getLastID();
+	
+	/**
+	 * Return the number of Posts currently in TextBook's collection.
+	 * 
+	 * @return current number of Posts
+	 */
+	public int getPostCount();
+	
+	/**
+	 * Returns the String from the indexed Post's toString(), which includes the Post
+	 * itself and its comments. Returns null if the index is invalid.
+	 * 
+	 * @param index index of Post
+	 * @return toString() from Post at given index or null if invalid index
+	 */
+	public String getPostString(int index);
+	
+	/**
+	 * Increments the value of last ID. This becomes the ID for the new Post.
+	 * Adds a new Post with the given author and text and the new ID to the
+	 * collection of Posts.
+	 * Appends the new Post ID to file POST_LIST_FILENAME.
+	 * 
+	 * @param author name of the new Post's author
+	 * @param text text of the new Post
+	 * @return true if successful, else false
+	 */
+	public boolean addPost(String author, String text);
+	
+	/**
+	 * Removes and returns the Post at the given index or returns null if index is 
+	 * invalid. If the index is valid, rewrites the file with name POST_LIST_FILENAME 
+	 * to include only the Posts still in the collection.
+	 * 
+	 * @param index the index of a Post in TextBook's collection
+	 * @return removed Post or null if index was invalid
+	 */
+	public Post removePost(int index);
+	
+	/**
+	 * Adds a new comment with the given author and text to the Post at the given
+	 * index if valid. If the given index is invalid, returns false.
+	 * 
+	 * @param postIndex index of post targeted by this comment
+	 * @param author name of comment's author
+	 * @param text text of the comment
+	 * @return true if index is valid, else false
+	 */
+	public boolean addComment(int postIndex, String author, String text);
+	
+	/**
+	 * Returns a well-formatted String with the current number of Posts and a list 
+	 * of Posts where each Post's toStringPostOnly() is preceded by its index in the 
+	 * collection and each post is on its own line as shown in the example.
+	 * Example:
+	 *   "TextBook contains 2 posts:
+	 *   0 - 00010 2021-11-02T12:20:59.122Z Mason Did stuff.
+	 *   1 - 00011 2021-11-02T12:45:52.023Z Luke Heckled Mason."
+	 * 
+	 * @return well-formatted String with post count and indexed posts
+	 */
+	public String toString();	
+	
+	/**
+	 * Returns a COPY of the posts list for testing. Make sure you are not violating
+	 * encapsulation by directly returning the posts variable or an alias of the
+	 * posts variable.
+	 * (Note that a copy of the list still violates ideal encapsulation because we’re 
+	 * not performing a ‘deep’ copy of the Post objects themselves. However, this is 
+	 * adequate for testing purposes.)
+	 * 
+	 * @return copy of posts list
+	 */
+	public ArrayList<Post> getPosts();
+
+}
